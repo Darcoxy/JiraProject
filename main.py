9e5c4c4ce3013f505f9eb92eb1456f2293f9d560
JJ <jj@anbast.com>
2022-11-01 14:16:15 +0000

    API Token Added

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-from collections import Counter
-from typing import cast
-from urllib import request, response
-
 from jira import JIRA
-from jira.client import ResultList
-from jira.resources import Issue
 
-url = "https://anbast.atlassian.net/browse/SR-1468"
+jira = JIRA('https://anbast.atlassian.net/')
 
-jira = JIRA(
-    basic_auth=("jj@anbast.com", "J$b#g!b4a"), 
-)
-
-headers = {
-    "Accept": "application/json"
-}
-
-response = requests.request(
-    "GET",
-    url,
-    headers=headers,
-    auth=jira
-)
-
-print(json.dumps(json.loads(reposnse.text), sort_keys=True, indent=4, separators=(",", ": ")))
+auth_jira = JIRA(basic_auth=('jj@anbast.com', 'SUGv050W7U8sbpoi7LyOF7EC'))
