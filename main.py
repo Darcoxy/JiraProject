063ca62c59176e14dcccf796ca04167739117a67
Darcoxy <post.lewandowski@gmail.com>
2022-11-02 11:44:27 +0000

    new token

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ Options = {
     'verify': True
 }
 
-jira = JIRA(options=Options, basic_auth=('jj@anbast.com', "EpuTwjVN6Un5g5Au5lOwFAF4"))
+jira = JIRA(options=Options, basic_auth=('jj@anbast.com', "3xKNITs31ZuyquzyXrIE9291"))
 
-filter = jira.filter(19012)
-print(filter)
+testFilter = jira.filter(19012)
+print(testFilter)
 
-""" data = Path("index.html").read_text().replace('\n', ' ')
+testFilter.update_filter(19012, 'project = PUR AND fixVersion >= 1.61.0 and fixVersion <= 1.63.77')
+
+data = Path("index.html").read_text().replace('\n', ' ')
 output = data[2:]
 patchVersion = output.split('<')[0]
 testVersion = output.split('>')[1].replace(" ", "")
-print(patchVersion)
-print(testVersion) """
