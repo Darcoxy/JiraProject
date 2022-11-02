63e3e0f2296aa0c9428d323565d5f6c4497b5f33
Darcoxy <post.lewandowski@gmail.com>
2022-11-02 12:38:41 +0000

    made changes

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ Options = {
     'verify': True
 }
 
-jira = JIRA(options=Options, basic_auth=('jj@anbast.com', "3xKNITs31ZuyquzyXrIE9291"))
+jira = JIRA(options=Options, basic_auth=('jj@anbast.com', "njJpXckRq80MU5kqmquH52F4"))
 
 testFilter = jira.filter(19012)
 print(testFilter)
 
-testFilter.update_filter(19012, 'project = PUR AND fixVersion >= 1.61.0 and fixVersion <= 1.63.77')
+#testFilter.update_filter(19012, 'project = PUR AND fixVersion >= 1.61.0 and fixVersion <= 1.63.77')
 
-data = Path("index.html").read_text().replace('\n', ' ')
+""" data = Path("index.html").read_text().replace('\n', ' ')
 output = data[2:]
 patchVersion = output.split('<')[0]
 testVersion = output.split('>')[1].replace(" ", "")
+ """
--- update_filter.py
+++ update_filter.py
@@ -2,9 +2,9 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = 'https://anbast.atlassian.net/'
+""" url = 'https://anbast.atlassian.net/rest/api/3/filter/19012'
 
-auth = HTTPBasicAuth('jj@anbast.com', 'PAArIcdYuTqSujYW8ylq69B7')
+auth = HTTPBasicAuth('jj@anbast.com', 'njJpXckRq80MU5kqmquH52F4')
 
 headers = {
     "Accept": "application/json",
@@ -13,7 +13,7 @@ headers = {
 
 payload = json.dumps( {
     'jql': 'type = project = PUR AND fixVersion >= 1.61.0 and fixVersion <= 1.63.77',
-    'name': 'UpdatedTestingJiraProject',
+    'name': 'TestingJiraProject',
     'description': 'Update from 02/11/2022'
 })
 
@@ -23,4 +23,51 @@ response = requests.request(
     data=payload,
     headers=headers,
     auth=auth
-)
+) """
+
+
+def getFilter():
+    url = 'https://anbast.atlassian.net/rest/api/3/filter/19012'
+
+    auth = HTTPBasicAuth('jj@anbast.com', 'njJpXckRq80MU5kqmquH52F4')
+
+    headers = {
+        'Accept': 'application/json'
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        auth=auth
+    )
+    print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
+def updateFilter():
+    url = 'https://anbast.atlassian.net/rest/api/3/filter/19012'
+
+    auth = HTTPBasicAuth("email@example.com", "<api_token>")
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+        "jql": "type = project = PUR AND fixVersion >= 1.61.0 and fixVersion <= 1.63.77",
+        "name": "UpdatedTestingJiraProject",
+        "description": "Lists all bugs for patch version"
+    })
+
+    response = requests.request(
+        "PUT",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+    )
+    print(response)
+    print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))) 
+
+#updateFilter()
+getFilter()
