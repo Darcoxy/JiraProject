8c16597684419cb807a4eb60adbc33c1f49f67f6
Darcoxy <post.lewandowski@gmail.com>
2022-11-02 10:48:30 +0000

    added new jira API token

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 jira = JIRA('https://anbast.atlassian.net/')
 
-#auth_jira = JIRA(basic_auth=('jj@anbast.com', 'SUGv050W7U8sbpoi7LyOF7EC'))
+auth_jira = JIRA(basic_auth=('jj@anbast.com', '7mOZ3JbO241wwaL0Qkk9C155'))
 
-data = Path("index.html").read_text().replace('\n', ' ')
+""" data = Path("index.html").read_text().replace('\n', ' ')
 output = data[2:]
 patchVersion = output.split('<')[0]
 testVersion = output.split('>')[1].replace(" ", "")
 print(patchVersion)
-print(testVersion)
+print(testVersion) """
