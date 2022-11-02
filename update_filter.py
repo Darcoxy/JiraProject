03f6bdb7256ab025164c4c16fcdeb128ac57f03f
Darcoxy <post.lewandowski@gmail.com>
2022-11-02 12:41:55 +0000

    new token

--- update_filter.py
+++ update_filter.py
@@ -2,30 +2,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-""" url = 'https://anbast.atlassian.net/rest/api/3/filter/19012'
-
-auth = HTTPBasicAuth('jj@anbast.com', 'njJpXckRq80MU5kqmquH52F4')
-
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-}
-
-payload = json.dumps( {
-    'jql': 'type = project = PUR AND fixVersion >= 1.61.0 and fixVersion <= 1.63.77',
-    'name': 'TestingJiraProject',
-    'description': 'Update from 02/11/2022'
-})
-
-response = requests.request(
-    "PUT",
-    url,
-    data=payload,
-    headers=headers,
-    auth=auth
-) """
-
-
 def getFilter():
     url = 'https://anbast.atlassian.net/rest/api/3/filter/19012'
 
@@ -46,7 +22,7 @@ def getFilter():
 def updateFilter():
     url = 'https://anbast.atlassian.net/rest/api/3/filter/19012'
 
-    auth = HTTPBasicAuth("email@example.com", "<api_token>")
+    auth = HTTPBasicAuth("email@example.com", "<lH37epa48lBGDcT3YkXW43B0>")
 
     headers = {
         "Accept": "application/json",
@@ -66,8 +42,8 @@ def updateFilter():
         headers=headers,
         auth=auth
     )
+    #print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))) 
     print(response)
-    print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))) 
 
-#updateFilter()
-getFilter()
+updateFilter()
+#getFilter()
