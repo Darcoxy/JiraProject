a017df7ef10cc654e126b03fb2d4c1b151d40ea4
Darcoxy <post.lewandowski@gmail.com>
2022-11-02 10:58:37 +0000

    Working with Jira again

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from jira import JIRA
+from ssl import Options
 from pathlib import Path
 
-jira = JIRA('https://anbast.atlassian.net/')
+Options = {
+    'server': 'https://anbast.atlassian.net/',
+    'verify': True
+}
 
-auth_jira = JIRA(basic_auth=('jj@anbast.com', '7mOZ3JbO241wwaL0Qkk9C155'))
+jira = JIRA(options=Options, basic_auth=('jj@anbast.com', "7mOZ3JbO241wwaL0Qkk9C155"))
+
+issue = jira.issue('PUR-12792')
+print(issue.key)
+print(issue.fields.summary)
 
 """ data = Path("index.html").read_text().replace('\n', ' ')
 output = data[2:]
