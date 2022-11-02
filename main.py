13588a408d6d8051b593f7503758a03e91323f22
Darcoxy <post.lewandowski@gmail.com>
2022-11-02 11:00:49 +0000

    New Jira API Token

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ Options = {
     'verify': True
 }
 
-jira = JIRA(options=Options, basic_auth=('jj@anbast.com', "7mOZ3JbO241wwaL0Qkk9C155"))
+jira = JIRA(options=Options, basic_auth=('jj@anbast.com', "1QqfZ5AYASQ9ydwU5J977675"))
 
-issue = jira.issue('PUR-12792')
+filter = jira.filter(18839)
+print(filter)
+""" issue = jira.issue('PUR-12792')
 print(issue.key)
-print(issue.fields.summary)
+print(issue.fields.summary) """
 
 """ data = Path("index.html").read_text().replace('\n', ' ')
 output = data[2:]
