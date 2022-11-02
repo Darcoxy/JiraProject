7c2e6e589ebbce6a2ec23cdbf66093d37507ddfc
Darcoxy <post.lewandowski@gmail.com>
2022-11-02 10:43:37 +0000

    Changed the version numbers to strings

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from jira import JIRA
+from pathlib import Path
 
 jira = JIRA('https://anbast.atlassian.net/')
 
-auth_jira = JIRA(basic_auth=('jj@anbast.com', 'SUGv050W7U8sbpoi7LyOF7EC'))
+#auth_jira = JIRA(basic_auth=('jj@anbast.com', 'SUGv050W7U8sbpoi7LyOF7EC'))
+
+data = Path("index.html").read_text().replace('\n', ' ')
+output = data[2:]
+patchVersion = output.split('<')[0]
+testVersion = output.split('>')[1]
+print(patchVersion)
+print(testVersion)
